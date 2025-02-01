21f03ee3c2079113982479de7bc20c9ea5d0eba9
AnasSAV <anashussaindeen@gmail.com>
2025-02-01 19:41:46 +0530

    Revert "add update product functionality and modify product routes"
    This reverts commit cc3649eb279ca11294c26405e49c872940e7ffca.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { authMiddleware } = require('../middlewares/authMiddleware');
-const productController = require('../controllers/productController');
+const { getAllProducts } = require('../controllers/productController');
+const { createProduct } = require('../controllers/productController');
+
 const router = express.Router();
 
 // Get all products (Protected Route)
-router.get('/products-get', authMiddleware, productController.getAllProducts);
-router.post('/products-create', authMiddleware, productController.createProduct);
-router.post('/products-update', authMiddleware, productController.updateProduct);
+router.get('/products-get', authMiddleware, getAllProducts);
+router.post('/products-create', authMiddleware, createProduct);
 
 module.exports = router;
