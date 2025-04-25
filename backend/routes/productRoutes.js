6a9fb56f61a6a5cfa12f6f8b5127acb645d9a951
AnasSAV <anashussaindeen@gmail.com>
2025-04-25 18:30:27 +0530

    feat: implement getDashboardStats endpoint to fetch various dashboard statistics

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
-import { getAllProducts, createProduct } from '../controllers/productController.js';
+import { 
+    getAllProducts, 
+    createProduct, 
+    getDashboardStats 
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.get('/products-get', authMiddleware, getAllProducts);
 router.post('/products-create', authMiddleware, createProduct);
+router.get('/dashboard-stats', authMiddleware, getDashboardStats);
 
 export default router;
